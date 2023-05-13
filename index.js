5f99496653d19cb619200260c5f3b32c275aeb57
aleixnit <nitjac210@gmail.com>
2023-05-13 13:57:52 +0200

    agrupo la logica en una misma funcion de 'operacion'

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ let resetearPantalla = false;
 //Recorro todos los botones con el metodo 'for Each', para ejecutar la lógica correspondiente cuando se hace 'click' a un botón.
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
-
     //Guardo en la variable buttonText el valor de la cadena que contiene cada boton, 1, 2, 3, +, -....
     const buttonText = event.target.textContent;
 
-    //Switch case según el valor del botton que se ha pulsado 
+    //Switch case según el valor del botton que se ha pulsado
     switch (buttonText) {
       case "RESET":
         result.textContent = "";
@@ -28,70 +27,38 @@ buttons.forEach((button) => {
         break;
       case "=":
         // Realizar el cálculo y mostrar el resultado
-        // Si numero1, operador y result estan 
+        // Si numero1, operador y result estan
         if (numero1 && operador && result.textContent !== "") {
-            numero2 = Number(result.textContent);
-            const resultadoOperacion = calcular(numero1, operador, numero2);
-            result.textContent = resultadoOperacion;
-            numero1 = resultadoOperacion;
-            operador = null;
-            numero2 = null;
-            resetearPantalla = true;
-          }
+          numero2 = Number(result.textContent);
+          const resultadoOperacion = calcular(numero1, operador, numero2);
+          result.textContent = resultadoOperacion;
+          numero1 = resultadoOperacion;
+          operador = null;
+          numero2 = null;
+          resetearPantalla = true;
+        }
         break;
       case "+":
-        if (numero1 !== null && numero2 !== null) {
-            const resultado = calcular(numero1, operador, numero2);
-            result.textContent = resultado;
-            numero1 = resultado;
-            numero2 = null;
-          }
-          numero1 = Number(result.textContent);
-          operador = buttonText;
-          result.textContent = "";
-          break;
+        operacion(buttonText);
+        break;
         // result.textContent = "";
         // result.textContent += buttonText;
         break;
       case "-":
-        if (numero1 !== null && numero2 !== null) {
-            const resultado = calcular(numero1, operador, numero2);
-            result.textContent = resultado;
-            numero1 = resultado;
-            numero2 = null;
-          }
-          numero1 = Number(result.textContent);
-          operador = buttonText;
-          result.textContent = "";
+        operacion(buttonText);
         break;
       case "x":
-        if (numero1 !== null && numero2 !== null) {
-            const resultado = calcular(numero1, operador, numero2);
-            result.textContent = resultado;
-            numero1 = resultado;
-            numero2 = null;
-          }
-          numero1 = Number(result.textContent);
-          operador = buttonText;
-          result.textContent = "";
+        operacion(buttonText);
         break;
       case "/":
-        if (numero1 !== null && numero2 !== null) {
-            const resultado = calcular(numero1, operador, numero2);
-            result.textContent = resultado;
-            numero1 = resultado;
-            numero2 = null;
-          }
-          numero1 = Number(result.textContent);
-          operador = buttonText;
-          result.textContent = "";
+        operacion(buttonText);
         break;
       default:
         if (resetearPantalla) {
-            result.textContent = "";
-            resetearPantalla = false;
-          }
-          result.textContent += buttonText;
+          result.textContent = "";
+          resetearPantalla = false;
+        }
+        result.textContent += buttonText;
         // Agregar el número o operador a la barra de resultados
         break;
     }
@@ -99,19 +66,31 @@ buttons.forEach((button) => {
 });
 
 function calcular(numero1, operador, numero2) {
-    switch (operador) {
-      case "+":
-        return numero1 + numero2;
-      case "-":
-        return numero1 - numero2;
-        break;
-      case "x":
-        return numero1 * numero2;
-        break;
-      case "/":
-        return numero1 / numero2;
-        break;
-      default:
-        return null;
-    }
+  switch (operador) {
+    case "+":
+      return numero1 + numero2;
+    case "-":
+      return numero1 - numero2;
+      break;
+    case "x":
+      return numero1 * numero2;
+      break;
+    case "/":
+      return numero1 / numero2;
+      break;
+    default:
+      return null;
   }
+}
+
+function operacion(buttonText) {
+  if (numero1 !== null && numero2 !== null) {
+    const resultado = calcular(numero1, operador, numero2);
+    result.textContent = resultado;
+    numero1 = resultado;
+    numero2 = null;
+  }
+  numero1 = Number(result.textContent);
+  operador = buttonText;
+  result.textContent = "";
+}
