d018d8978550235cbfd67a77ce3a6fc34a425825
aleixnit <nitjac210@gmail.com>
2023-05-13 12:43:06 +0200

    boton de borrar funcional

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const buttons = document.querySelectorAll('.keyboard button');
-const result = document.querySelector('#result');
+const buttons = document.querySelectorAll(".keyboard button");
+const result = document.querySelector("#result");
+let numero1 = null;
+let numero2 = null;
 
-buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
-      const buttonText = event.target.textContent;
-      
-      switch (buttonText) {
-        case 'RESET':
-          result.textContent = ''; 
-          // Reiniciar el resultado
-          break;
-        case '=':
-          // Realizar el cálculo y mostrar el resultado
-          break;
-        default:
-          result.textContent += buttonText; 
-          // Agregar el número o operador al resultado
-          break;
-      }
-    });
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const buttonText = event.target.textContent;
+
+    switch (buttonText) {
+      case "RESET":
+        result.textContent = "";
+        // Reiniciar el resultado
+        break;
+      case "DEL":
+        result.textContent = result.textContent.slice(0, -1);
+        break;
+      case "=":
+        // Realizar el cálculo y mostrar el resultado
+        break;
+      case "+":
+        break;
+      case "-":
+        break;
+      case "x":
+        break;
+      case "/":
+        break;
+      default:
+        result.textContent += buttonText;
+        // Agregar el número o operador al resultado
+        break;
+    }
   });
+});
